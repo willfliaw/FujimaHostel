9f59b9a4a56754589f9121c666b765dba22e5ff6
William Liaw <willfliaw@gmail.com>
2025-01-02 19:51:36 +0100

    hostelworld refs

--- js/iconFetcher.js
+++ js/iconFetcher.js
@@ -1,21 +1,45 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const bookingIconContainers = document.querySelectorAll(".booking-icon-container");
+// Booking.com Icon Injection
+const bookingIconContainers = document.querySelectorAll(
+    ".booking-icon-container"
+);
 
-    fetch("https://cf.bstatic.com/static/img/favicon/9ca83ba2a5a3293ff07452cb24949a5843af4592.svg")
-        .then(response => response.text())
-        .then(data => {
-            bookingIconContainers.forEach(container => {
-                container.innerHTML = data;
-                const svgElement = container.querySelector("svg");
-                svgElement.classList.add("booking-icon");
+fetch("images/booking.svg")
+    .then((response) => response.text())
+    .then((data) => {
+        bookingIconContainers.forEach((container) => {
+            container.innerHTML = data;
+            const svgElement = container.querySelector("svg");
+            svgElement.classList.add("booking-icon");
 
-                // Remove inline styles
-                svgElement.removeAttribute("style");
+            // Remove inline styles
+            svgElement.removeAttribute("style");
 
-                // Remove any <style> blocks inside the SVG
-                const styles = svgElement.querySelectorAll("style");
-                styles.forEach(style => style.remove());
-            });
-        })
-        .catch(error => console.error("Error loading SVG:", error));
-});
+            // Remove any <style> blocks inside the SVG
+            const styles = svgElement.querySelectorAll("style");
+            styles.forEach((style) => style.remove());
+        });
+    })
+    .catch((error) => console.error("Error loading SVG:", error));
+
+// Hostelworld Icon Injection
+const hostelworldIconContainers = document.querySelectorAll(
+    ".hostelworld-icon-container"
+);
+
+fetch("images/hw-icon.svg")
+    .then((response) => response.text())
+    .then((data) => {
+        hostelworldIconContainers.forEach((container) => {
+            container.innerHTML = data;
+            const svgElement = container.querySelector("svg");
+            svgElement.classList.add("hostelworld-icon");
+
+            // Remove inline styles
+            svgElement.removeAttribute("style");
+
+            // Remove any <style> blocks inside the SVG
+            const styles = svgElement.querySelectorAll("style");
+            styles.forEach((style) => style.remove());
+        });
+    })
+    .catch((error) => console.error("Error loading Hostelworld SVG:", error));
